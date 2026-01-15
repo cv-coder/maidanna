7503277f876db90439010f11bd76745b52dcb8b8
普通市民梁生 <1874764803@qq.com>
2026-01-15 18:43:49 +0800

    fix：init

--- book-store/vite.config.ts
+++ book-store/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+ 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/book-store/', // TODO: 如果部署到 https://<USERNAME>.github.io/<REPO>/，请将此处的 '/book-store/' 替换为你的仓库名 '/<REPO>/'
