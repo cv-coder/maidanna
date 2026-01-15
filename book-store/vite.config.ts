3f900000125c2b2a97fec1efaaa8d68d2ccf16ef
普通市民梁生 <1874764803@qq.com>
2026-01-15 18:41:55 +0800

    feat：cicd github action配置

--- book-store/vite.config.ts
+++ book-store/vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/book-store/', // TODO: 如果部署到 https://<USERNAME>.github.io/<REPO>/，请将此处的 '/book-store/' 替换为你的仓库名 '/<REPO>/'
   plugins: [vue()],
 })
